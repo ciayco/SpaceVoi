6de76215be23f3fcc45eab657260516f3792d233
Emre A <emreatli060@gmail.com>
2016-01-30 18:18:41 +0200

    optimizasyon saat 18:15

--- app/src/main/java/com/example/ciayco/spacevoi/MainActivity.java
+++ app/src/main/java/com/example/ciayco/spacevoi/MainActivity.java
@@ -141,8 +141,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
 ////sonradan eklenen
         pDialog = new ProgressDialog(this);
         pDialog.setIndeterminate(false);
