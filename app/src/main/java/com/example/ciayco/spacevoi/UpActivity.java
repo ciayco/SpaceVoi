4211020e50a5de5bbe23576d476ddfed3cac9b2b
Ciayco <ciayco07@gmail.com>
2016-03-11 00:43:52 +0200

    ++Alpha Ver 1.3

---
+++ app/src/main/java/com/example/ciayco/spacevoi/UpActivity.java
@@ -0,0 +1,118 @@
+package com.example.ciayco.spacevoi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+import java.util.Date;
+
+
+public class UpActivity extends AppCompatActivity {
+
+    //region Tanımlamalar
+    Upload us = new Upload();
+    SesKayit ka = new SesKayit();
+    static String loggedUser;
+    static String kayitkodu;
+
+
+
+    //endregion
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.up_activity);
+
+        //region Bar Oluştur
+
+        android.support.v7.app.ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater mInflater = LayoutInflater.from(this);
+        View mCustomView = mInflater.inflate(R.layout.ana_bar, null);
+        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.baslik);
+        mTitleTextView.setText("SpaceVoi");
+        mActionBar.setCustomView(mCustomView);
+        mActionBar.setDisplayShowCustomEnabled(true);
+
+        //endregion
+
+
+        //region Login
+        Intent intent = getIntent();
+        Bundle intentBundle = intent.getExtras();
+        loggedUser = intentBundle.getString("USERNAME");
+        loggedUser = loggedUser.toLowerCase();
+        loggedUser = loggedUser.substring(0,1).toUpperCase()+ loggedUser.substring(1);
+
+        final TextView loginUsername = (TextView)findViewById(R.id.login_user);
+
+        loginUsername.setText("Hoşgeldin " + loggedUser);
+
+        //endregion
+
+
+        //region Butonlar
+
+
+        //Kaydet Click
+        final Button kayit = (Button)findViewById(R.id.kaydetbut);
+
+        kayit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ka.kayitkontrol()) {
+                    Date zaman = new Date();
+                    String damga = Long.toString(zaman.getTime());
+                    kayitkodu = loggedUser + damga;
+                    ka.startRecording(kayitkodu);
+                    kayit.setText("Kaydediliyor");
+                } else {
+                    ka.stopRecording();
+                    kayit.setText("Kaydet");
+                }
+            }
+        });
+
+        //Gönder Buton
+        final Button gonder = (Button)findViewById(R.id.gonderbut);
+        gonder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                us.DosyaGonder(getApplicationContext(), loggedUser, kayitkodu);
+            }
+        });
+
+        //Action Bar Button
+        ImageButton mainbut = (ImageButton) mCustomView
+                .findViewById(R.id.mainmenu);
+        mainbut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(UpActivity.this, MainActivity.class);
+                intent.putExtra("USERNAME", loggedUser);
+                startActivity(intent);
+            }
+        });
+
+
+        //endregion
+    }
+}
